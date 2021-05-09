90d4f602bbfac4fca0ce784962239521c2a11097
Pacil142857 <7coolaustin7@gmail.com>
2021-05-09 18:34:58 -0400

    Added support for texting images with the new Sender.text_image method. Created Sender.start and Sender.quit methods. This will improve performance when sending multiple text messages.

--- send_text.py
+++ send_text.py
@@ -1,9 +1,11 @@
-import csv
+import os
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
 from smtplib import SMTP, SMTP_SSL, SMTPConnectError
 
 
 # Credit for creating (most of) this: https://github.com/acamso/demos/blob/master/_email/send_txt_msg.py
-CARRIER_MAP = {
+SMS_CARRIER_MAP = {
     'verizon': 'vtext.com',
     'tmobile': 'tmomail.net',
     't-mobile': 'tmomail.net',
@@ -13,6 +15,18 @@ CARRIER_MAP = {
     'cricket': 'sms.cricketwireless.net',
     'uscellular': 'email.uscc.net',
     'us cellular': 'email.uscc.net'
+}
+
+MMS_CARRIER_MAP = {
+    'verizon': 'vzwpix.com',
+    'tmobile': 'tmomail.net',
+    't-mobile': 'tmomail.net',
+    'sprint': 'pm.sprint.com',
+    'at&t': 'mms.att.net',
+    'boost': 'myboostmobile.com',
+    'cricket': 'mms.cricketwireless.net',
+    'uscellular': 'mms.uscc.net',
+    'us cellular': 'mms.uscc.net'
 }
 
 # A list of dicts of smtp servers, their email extensions, and their port number
@@ -32,9 +46,9 @@ class CouldNotConnectToServer(Exception):
     pass
 class InvalidRecipient(Exception):
     pass
-class NoSMTPServer(Exception):
-    pass
 class NoPortNumber(Exception):
+    pass
+class NoSMTPServer(Exception):
     pass
 
 
@@ -120,6 +134,39 @@ class Sender:
             self.port = port
     
 
+    def start(self):
+        '''Connects to the SMTP server and logs in.'''
+        # If the port is 587, start with TLS
+        if self.port == 587:
+            self.server = SMTP(self.smtp_server, self.port)
+            self.server.starttls()
+        
+        # If the port is 465, start with SSL
+        elif self.port == 465:
+            self.server = SMTP_SSL(self.smtp_server, self.port)
+        
+        # Try both TLS and SSL
+        else:
+            try:
+                self.server = SMTP(self.smtp_server, self.port)
+            except SMTPConnectError:
+                self.server = SMTP_SSL(self.smtp_server, self.port)
+            else:
+                self.server.starttls()
+
+        # Confirm that the server is connected
+        if self.server.ehlo()[0] != 250:
+            raise CouldNotConnectToServer('Failed to connect to the SMTP server.')
+        
+        # Log in to the email address
+        self.server.login(self.email, self.passwd)
+    
+    
+    def quit(self):
+        '''Disconnects from the SMTP server.'''
+        self.server.quit()
+
+
     def text(self, recipient, message, carrier=None):
         '''
         Send a text message from the Sender's email.
@@ -133,7 +180,6 @@ class Sender:
         
         Raises:
             CarrierNotFound: A carrier was provided, but it couldn't be found. Please look up the SMS gateway domain of the carrier and include it in the recipient argument.
-            CouldNotConnectToServer: The SMTP server could not maintain a connection.
             InvalidRecipient: The recipient doesn't have an @ symbol in it (and therefore does not contain the SMS gateway domain), and no carrier is provided.
         
         Returns:
@@ -150,37 +196,58 @@ class Sender:
             carrier = carrier.lower()
             
             # If the carrier is found, change the recipient to be a valid SMS gateway
-            if carrier in CARRIER_MAP:
-                recipient += '@' + CARRIER_MAP[carrier]
+            if carrier in SMS_CARRIER_MAP:
+                recipient += '@' + SMS_CARRIER_MAP[carrier]
             # Carrier wasn't found, raise an exception
             else:
                 raise CarrierNotFound('A carrier wasn\'t found. Try changing the recipient to include the SMS gateway domain.')
         
-        # If the port is 587, start with TLS
-        if self.port == 587:
-            server = SMTP(self.smtp_server, self.port)
-            server.starttls()
+        # Send the mail
+        self.server.sendmail(self.email, recipient, 'Subject:\n\n' + message) # TODO: This might be vulnerable to something similar to a SQLi attack. Prevent that from happening.
+    
+    
+    def text_image(self, recipient, image, carrier=None):
+        '''
+        Text the recipient an image from the Sender's email.
         
-        # If the port is 465, start with SSL
-        elif self.port == 465:
-            server = SMTP_SSL(self.smtp_server, self.port)
+        Arguments:
+            recipient (str): The phone number to be texted. It's recommended that it includes the MMS gateway domain (e.g. "1234567890@vzwpix.com"). Otherwise, the format should be "1234567890".
+            image (str): The path of the image to be sent. Ex: "./path/to/image.png"
         
-        # Try both TLS and SSL
-        else:
-            try:
-                server = SMTP(self.smtp_server, self.port)
-            except SMTPConnectError:
-                server = SMTP_SSL(self.smtp_server, self.port)
+        Keyword arguments:
+            carrier (str): The carrier of the recipient (e.g. Verizon). Only a few of the most popular carriers are supported; including the MMS gateway domain in recipient is recommended over using this.
+        
+        Raises:
+            CarrierNotFound: A carrier was provided, but it couldn't be found. Please look up the MMS gateway domain of the carrier and include it in the recipient argument.
+            InvalidRecipient: The recipient doesn't have an @ symbol in it (and therefore does not contain the MMS gateway domain), and no carrier is provided.
+        
+        Returns:
+            None
+        '''
+    
+        # The phone number isn't complete, so the program tries to find what's missing
+        if '@' not in recipient:
+            
+            # A carrier isn't provided, so nothing can be done. Raise an exception.
+            if not carrier:
+                raise InvalidRecipient('The recipient of the text message is invalid. It have an @ symbol in it and look like an email address.')
+            
+            carrier = carrier.lower()
+            
+            # If the carrier is found, change the recipient to be a valid SMS gateway
+            if carrier in MMS_CARRIER_MAP:
+                recipient += '@' + MMS_CARRIER_MAP[carrier]
+            # Carrier wasn't found, raise an exception
             else:
-                server.starttls()
-
-        # Confirm that the server is connected
-        if server.ehlo()[0] != 250:
-            raise CouldNotConnectToServer('Failed to connect to the SMTP server.')
+                raise CarrierNotFound('A carrier wasn\'t found. Try changing the recipient to include the MMS gateway domain.')
         
-        # Log in to the email address
-        server.login(self.email, self.passwd)
+        # Get the image data
+        with open(image, 'rb') as img:
+            img_data = img.read()
+        
+        # Attach the image to a blank message
+        msg = MIMEMultipart()
+        msg.attach(MIMEImage(img_data, name=os.path.basename(image)))
         
         # Send the mail
-        server.sendmail(self.email, recipient, 'Subject:\n\n' + message) # TODO: This might be vulnerable to something similar to a SQLi attack. Prevent that from happening.
-        server.quit()
+        self.server.sendmail(self.email, recipient, msg.as_string())
