6b6d862ed5e592ac2fc6118756734f521804b3b4
Pacil142857 <7coolaustin7@gmail.com>
2021-05-12 14:42:13 -0400

    Created setup.py

---
+++ setup.py
@@ -0,0 +1,46 @@
+# MIT License
+
+# Copyright (c) 2021 Austin McGregor
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from setuptools import setup
+
+with open('README.md') as f:
+    long_desc = f.read()
+
+setup(
+    name='send_text',
+    version='1.0.0',
+    description='A simple Python module to send text messages',
+    long_description=long_desc,
+    long_description_content_type='text/markdown',
+    url='https://github.com/Pacil142857/send_text',
+    author='Austin McGregor',
+    author_email='austinmcgregor2@gmail.com',
+    license='MIT',
+    license_files = ('LICENSE',),
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7'
+    ],
+    py_modules=['send_text'],
+    python_requires='>=3.5'
+)
